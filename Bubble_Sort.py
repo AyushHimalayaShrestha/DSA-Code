213aa2f52ede8d5ed4a6078690eccaef3ae66d73
Ayush  Shrestha <aayush625@gmail.com>
2025-03-07 17:02:14 +0545

    bubble sort

---
+++ Bubble_Sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(arr):
+    n = len(arr)  # Get the length of the array
+    for i in range(n):  # Outer loop to iterate through the array
+        for j in range(0, n-i-1):  # Inner loop to compare adjacent elements
+            if arr[j] > arr[j+1]:  # Check if the current element is greater than the next
+                arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap elements if the condition is true
+
+# Example usage
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Bubble Sorted Array:", arr)
